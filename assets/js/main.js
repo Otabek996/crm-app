f026f79fe68def7932f3799f87c8236086dd9b72
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-12-27 00:35:32 +0500

    feat: add bsic layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,27 @@ icons.forEach((icon) => {
         eyeOff.classList.toggle("eye-off");
     });
 })
+
+/*==================== ACTIVE LINK ====================*/
+const navbarItems = document.querySelectorAll(".navbar-item");
+let url = window.location.href.split("#")[1];
+
+function activeLink (link) {
+  for (let i = 0; i < navbarItems.length - 2; i++) {
+    navbarItems[i].classList.remove("active-link");
+  }
+  link.classList.add("active-link");
+  url = window.location.href;
+}
+
+if (url != undefined) {
+  for (let i = 0; i < navbarItems.length - 2; i++) {
+    if (url === navbarItems[i].id) {
+      navbarItems[i].classList.add("active-link");
+    }
+  }
+}
+
+for (let i = 0; i < navbarItems.length - 2; i++) {
+  navbarItems[i].addEventListener("click", () => activeLink(navbarItems[i]));
+}
