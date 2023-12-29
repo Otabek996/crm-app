cb04e89220c44169b873f517a06916b61b3172fd
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-12-29 14:50:46 +0500

    feat: add responsive layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,25 @@ if (url != undefined) {
 for (let i = 0; i < navbarItems.length - 2; i++) {
   navbarItems[i].addEventListener("click", () => activeLink(navbarItems[i]));
 }
+
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navOpen = document.getElementById('nav-open'),
+      navClose = document.getElementById('nav-close'),
+      backgroundShadow = document.getElementById('background-shadow'),
+      header = document.getElementById('header');
+
+/*===== MENU SHOW =====*/
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+      navMenu.classList.toggle('show-menu');
+      navOpen.classList.toggle('icon-off');
+      navClose.classList.toggle('icon-on');
+      header.classList.toggle('menu-on');
+      backgroundShadow.classList.toggle('disable-background-shadow');
+      document.querySelector('html').classList.toggle('no-scroll');
+  });
+}
+
+
